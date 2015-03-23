7054a342454c00c6a0a34dd889ea922e2484cbb7
Simon Wörpel <simon.woerpel@gmedienrevolte.de>
2015-03-23 22:33:52 +0100

    settings fix for django 1.6.11

--- koeln_api_wiki/settings.py
+++ koeln_api_wiki/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'mptt',
     'sekizai',
     'sorl.thumbnail',
+    'south',
     'theme',
     'wiki',
     'wiki.plugins.attachments',
@@ -55,7 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
