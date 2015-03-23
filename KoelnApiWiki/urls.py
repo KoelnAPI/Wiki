4e3a1d90a6a153d53d9959706e263f4eb43e7355
marcelbelledin <marcelbelledin@o2online.de>
2015-03-23 21:42:47 +0100

    hello new KoelnApiWiki

---
+++ KoelnApiWiki/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'KoelnApiWiki.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+from wiki.urls import get_pattern as get_wiki_pattern
+from django_nyt.urls import get_pattern as get_nyt_pattern
+urlpatterns += patterns('',
+    (r'^notifications/', get_nyt_pattern()),
+    (r'', get_wiki_pattern())
+)
